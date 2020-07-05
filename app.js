4e6db8f47cb7fe2d7f34e69655793491639ebf64
mba13 <mba13@mba13ui-MacBook-Air.local>
2020-07-05 17:37:06 +0900

    #2.12 Recap

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet());
 app.use(morgan("dev"));
+
 app.use(routes.home,globalRouter);
 app.use(routes.users,userRouter);
 app.use(routes.videos,videoRouter);
